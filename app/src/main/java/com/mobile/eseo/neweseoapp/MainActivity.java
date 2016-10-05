aaf098c6acbfc05558bd45b67790c822c8892fd7
rotab <jbd0@hotmail.fr>
2016-10-05 05:35:33 -0400

    test commit jb

--- app/src/main/java/com/mobile/eseo/neweseoapp/MainActivity.java
+++ app/src/main/java/com/mobile/eseo/neweseoapp/MainActivity.java
@@ -7,7 +7,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState); /////bloup
+        super.onCreate(savedInstanceState); ////JBBB
         setContentView(R.layout.activity_main);
     }
 }
