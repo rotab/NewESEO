99b0e32e22934a62d3d99af804b33ba355310819
boutanch <boutant.chloe@live.fr>
2016-10-10 10:48:58 +0200

    Ajout du tabhost et de l'icon newspaper

--- app/src/main/java/com/mobile/eseo/neweseoapp/MainActivity.java
+++ app/src/main/java/com/mobile/eseo/neweseoapp/MainActivity.java
@@ -1,13 +1,48 @@
 package com.mobile.eseo.neweseoapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.app.TabActivity;
 import android.os.Bundle;
+import android.widget.TabHost;
 
-public class MainActivity extends AppCompatActivity {
+@SuppressWarnings("deprecation")
+public class MainActivity extends TabActivity {
+
+    TabHost TabHostWindow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState); ////JBBBBe
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Assign id to Tabhost.
+        TabHostWindow = (TabHost) findViewById(android.R.id.tabhost);
+
+        //Creating tab menu.
+        TabHost.TabSpec TabMenu1 = TabHostWindow.newTabSpec("First tab");
+        TabHost.TabSpec TabMenu2 = TabHostWindow.newTabSpec("Second Tab");
+        TabHost.TabSpec TabMenu3 = TabHostWindow.newTabSpec("Third Tab");
+
+        //Setting up tab 1 name.
+        TabMenu1.setIndicator("Tab1");
+        //Set tab 1 activity to tab 1 menu.
+        TabMenu1.setContent(new Intent(this, TabActivity_1.class));
+
+        //Setting up tab 2 name.
+        TabMenu2.setIndicator("Tab2");
+        //Set tab 3 activity to tab 1 menu.
+        TabMenu2.setContent(new Intent(this, TabActivity_2.class));
+
+        //Setting up tab 2 name.
+        TabMenu3.setIndicator("Tab3");
+        //Set tab 3 activity to tab 3 menu.
+        TabMenu3.setContent(new Intent(this, TabActivity_3.class));
+
+        //Adding tab1, tab2, tab3 to tabhost view.
+
+        TabHostWindow.addTab(TabMenu1);
+        TabHostWindow.addTab(TabMenu2);
+        TabHostWindow.addTab(TabMenu3);
+
     }
 }
---
+++ app/src/main/java/com/mobile/eseo/neweseoapp/TabActivity_3.java
@@ -0,0 +1,13 @@
+package com.mobile.eseo.neweseoapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TabActivity_3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tab_3);
+    }
+}
